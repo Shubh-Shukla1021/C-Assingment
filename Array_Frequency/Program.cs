55dd7c1e146cde4c5ae6917ab402ee85e0d37917
Shubham <shubh.shukla1021@gmail.com>
2021-04-08 12:08:07 +0530

    first commit

---
+++ Array_Frequency/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Array_Frequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 8,1,1,3,8,4,5,2,3,2 };
+           
+            int[] arr1 = new int[arr.Length];
+            int exist = -1;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        
+                        arr1[j] = exist;
+                    }
+                }
+                if (arr1[i] != exist)
+                    arr1[i] = count;
+            }
+
+            
+            Console.WriteLine(" Element Frequency is");
+            
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] != exist)
+                    Console.WriteLine(arr[i] + "   frequency is   " + arr1[i]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace assingment
+{
+    class Program
+    {
+
+        enum Colors
+        {
+            Red,
+            Blue,
+            White,
+            Black,
+            Grey
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a color:");
+            string color = Console.ReadLine();
+            Colors value = (Colors)Enum.Parse(typeof(Colors), color);
+            switch (value)
+            {
+                case Colors.Red:
+                    Console.WriteLine("You have given Red ");
+                    break;
+                case Colors.Blue:
+                    Console.WriteLine("You have given Blue");
+                    break;
+                case Colors.White:
+                    Console.WriteLine("You have given White");
+                    break;
+                case Colors.Black:
+                    Console.WriteLine("You have given Black");
+                    break;
+                case Colors.Grey:
+                    Console.WriteLine("You have given Grey");
+                    break;
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
